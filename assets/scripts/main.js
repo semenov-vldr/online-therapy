02818147a825bce5203b67f5d2dd57d27624910e
semenov-vldr <semenov-vldr@users.noreply.github.com>
2025-02-19 22:32:56 +0000

    deploy: cb551b95b48a17b9fb0d4c7ec18b644ae7e28848

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -401,7 +401,7 @@ if (articles) {
     });
     document.getElementById("articles-top").scrollIntoView();
   };
-  var cardsPerPage = 4;
+  var cardsPerPage = 6;
   var articlesNav = articles.querySelector(".articles__nav");
   var prevButton = articlesNav.querySelector(".articles__nav-prev");
   var nextButton = articlesNav.querySelector(".articles__nav-next");
